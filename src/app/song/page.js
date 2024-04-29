8d2050189aa51af5f2ab693d459395cf7e393ae2
Ester Cortes <002escor@gmail.com>
2024-04-29 19:41:23 +0200

    feat: add image

--- src/app/song/page.js
+++ src/app/song/page.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 const Song = () => {
-	let song = "B.I for now";
-	let link =
-		"https://www.billboard.com/wp-content/uploads/2022/05/B.I-press-2022-131-photo-billboard-1548.jpg";
-	return (
-		<>
-			<h2>You get to listen to</h2>
-			<h1>{song}</h1>
-		</>
-	);
+  let song = "B.I for now";
+  let link =
+    "https://www.billboard.com/wp-content/uploads/2022/05/B.I-press-2022-131-photo-billboard-1548.jpg";
+  return (
+    <>
+      <h2>You get to listen to</h2>
+      <h1>{song}</h1>
+      <img src={link} />
+    </>
+  );
 };
 
 export default Song;
